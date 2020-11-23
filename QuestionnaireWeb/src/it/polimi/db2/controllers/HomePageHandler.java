05932f727ed5148b36019a7f4b342113a83490ea
Sergio <sergiopiermario.placanica@mail.polimi.it>
2020-11-23 23:51:57 +0100

    webServer initialization, added Thymeleaf

---
+++ QuestionnaireWeb/src/it/polimi/db2/controllers/HomePageHandler.java
@@ -0,0 +1,47 @@
+package it.polimi.db2.controllers;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@WebServlet("/Home")
+public class HomePageHandler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+
+	public HomePageHandler() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String resourcePath = getServletContext().getContextPath() + "/index.html";
+		System.err.println(resourcePath);
+		HttpSession session = req.getSession();
+		if (session.isNew() || session.getAttribute("user") == null) {
+			res.sendRedirect(resourcePath);
+			return;
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+	public void destroy() {
+	}
+}
