0c44dac0c8914e4a512f26dcbde895f4f72280f4
deep <pardeep.kumar@studenti.unipd.it>
2020-11-27 19:07:51 +0100

    Changes to the graphics Changed the homepage's structure, added greetings.html, changed the add marketing questions in createQuestionnaire.html and other small changes.

--- QuestionnaireWeb/WebContent/js/main.js
+++ QuestionnaireWeb/WebContent/js/main.js
@@ -1,33 +1,49 @@
 const form = document.getElementById('addForm');
-var itemsList=document.getElementById('items');
-form.addEventListener("submit",addItem);
-itemsList.addEventListener('click',removeItem);
+var itemsList = document.getElementById('items');
+
+form.addEventListener("submit", addItem);
+itemsList.addEventListener('click', removeItem);
 
 
 
-function addItem(e){
+function addItem(e) {
     e.preventDefault();
     console.log(1);
-    var newItem=document.getElementById("question").value;
-    var li=document.createElement('li');
-    li.className="list-group-item";
-    console.log(li);
+    var newItem = document.getElementById("question").value;
+
+    var div = document.createElement('div');
+    div.className = "form-row";
+
+    var col = document.createElement("div");
+    col.className = "col-11";
+
+    var col1 = document.createElement("div");
+    col1.className = "col-1";
+
+    var li = document.createElement('li');
+    li.className = "list-group-item";
+
+    div.appendChild(col);
+    div.appendChild(col1);
+    col.appendChild(li);
     li.appendChild(document.createTextNode(newItem));
+    console.log(div);
 
-    var deleteBtn= document.createElement('button');
-    deleteBtn.className='btn btn-danger btn-sm float-right delete';
-    deleteBtn.appendChild(document.createTextNode('X'));
-    li.appendChild(deleteBtn);
 
-    itemsList.appendChild(li);
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'deleteBtn';
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+    col1.appendChild(deleteBtn);
+
+    itemsList.appendChild(div);
 
 }
 
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-        if(confirm('Are you sure?')){
-            var li=e.target.parentElement;
-            itemsList.removeChild(li);
+function removeItem(e) {
+    if (e.target.classList.contains('deleteBtn')) {
+        if (confirm('Are you sure?')) {
+            var row = e.target.parentElement.parentElement;
+            itemsList.removeChild(row);
         }
     }
 
