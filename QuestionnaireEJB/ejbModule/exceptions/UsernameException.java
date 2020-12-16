8ccf3694caa3ad8e8f1d2490b41609050c365a0a
deep <pardeep.kumar@studenti.unipd.it>
2020-12-16 16:38:03 +0100

    Changed how to get entity manager
    Now the entity manager it's retrieved with injection

--- QuestionnaireEJB/ejbModule/exceptions/UsernameException.java
+++ QuestionnaireEJB/ejbModule/exceptions/UsernameException.java
@@ -2,6 +2,9 @@ package exceptions;
 
 public class UsernameException extends Exception {
 
+
+	private static final long serialVersionUID = 1L;
+
 	public UsernameException(String message) {
 		super(message);
 	}
--- QuestionnaireEJB/ejbModule/services/UserService.java
+++ QuestionnaireEJB/ejbModule/services/UserService.java
@@ -12,23 +12,20 @@ import model.User;
 
 @Stateless
 public class UserService {
-	EntityManagerFactory emf = Persistence.createEntityManagerFactory("QuestionnaireEJB");
-	EntityManager em = emf.createEntityManager();
-//	@PersistenceContext(unitName = "QuestionnaireEJB")
-//	private EntityManager em;
+	@PersistenceContext(unitName = "QuestionnaireEJB")
+	private EntityManager em;
 	
 	public void createUser(String username, String password, String email) throws UsernameException {
 		System.out.println(em);
-		em.getTransaction().begin();
 		User user = new User(username, password, email);
 		user.setAdmin(false);
 		user.setBlocked(false);
-		em.persist(user);
 		try {
-			em.getTransaction().commit();
+			em.persist(user);
 		}
 		catch(Exception e){
 			throw new UsernameException("This username has been already used");
 		}
+
 	}
 }
